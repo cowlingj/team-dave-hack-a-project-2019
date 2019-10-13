fcd360db5785763c8775d821d5c67a2d6ffc6103
xChiri <chirilutageorge@yahoo.com>
2019-10-13 14:21:41 +0100

    Add endpoints for events

--- server/src/main/java/local/dave/hackathon/controllers/EventsController.java
+++ server/src/main/java/local/dave/hackathon/controllers/EventsController.java
@@ -1,9 +1,16 @@
 package local.dave.hackathon.controllers;
 
 import local.dave.hackathon.entities.Event;
+import local.dave.hackathon.entities.User;
+import local.dave.hackathon.entities.UserEventMap;
 import local.dave.hackathon.services.EventService;
+import local.dave.hackathon.services.UserEventMapService;
+import local.dave.hackathon.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/event")
@@ -11,6 +18,12 @@ public class EventsController {
 
     @Autowired
     private EventService eventService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserEventMapService userEventMapService;
 
     @GetMapping("/ping")
     public String ping() {
@@ -20,5 +33,20 @@ public class EventsController {
     @PostMapping
     public Event createEvent(@RequestBody Event event) {
         return eventService.save(event);
+    }
+
+    @GetMapping("/{eventId}/friends")
+    public List<User> getEventFriends(@PathVariable("eventId") Integer eventId) {
+        Event event = eventService.findEventById(eventId);
+        return event.getUserEventMaps().stream().map(UserEventMap::getUser).collect(Collectors.toList());
+    }
+
+    @PostMapping("/{eventId}/addFriend")
+    public UserEventMap addUserToEvent(@PathVariable("eventId") Integer eventId, @RequestParam("username") String username) {
+        User user = userService.findUser(username);
+        Event event = eventService.findEventById(eventId);
+
+        UserEventMap userEventMap = eventService.createUserEventMap(user, event, false);
+        return userEventMapService.save(userEventMap);
     }
 }
--- server/src/main/java/local/dave/hackathon/data/DataLoader.java
+++ server/src/main/java/local/dave/hackathon/data/DataLoader.java
@@ -1,7 +1,10 @@
 package local.dave.hackathon.data;
 
+import local.dave.hackathon.entities.Event;
 import local.dave.hackathon.entities.User;
+import local.dave.hackathon.entities.UserEventMap;
 import local.dave.hackathon.services.EventService;
+import local.dave.hackathon.services.UserEventMapService;
 import local.dave.hackathon.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -16,6 +19,9 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     
     @Autowired
     private EventService eventService;
+
+    @Autowired
+    private UserEventMapService userEventMapService;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -34,5 +40,24 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         user1 = userService.save(user1);
         user2 = userService.save(user2);
         user3 = userService.save(user3);
+
+        Event event1 = new Event();
+        event1.setName("Facebook Hack-a-project");
+        event1.setLocation("The Studio");
+        event1 = eventService.save(event1);
+
+        Event event2 = new Event();
+        event2.setName("Great Uni Hack");
+        event2.setLocation("MMU Business School");
+        event2 = eventService.save(event2);
+
+        UserEventMap userEventMap1 = eventService.createUserEventMap(user2, event1, false);
+        UserEventMap userEventMap2 = eventService.createUserEventMap(user3, event1, false);
+
+        UserEventMap userEventMap3 = eventService.createUserEventMap(user2, event2, false);
+
+        userEventMapService.save(userEventMap1);
+        userEventMapService.save(userEventMap2);
+        userEventMapService.save(userEventMap3);
     }
 }

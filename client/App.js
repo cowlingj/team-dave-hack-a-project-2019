cc4103719647524167ae6aebc02cf3643e918555
Katie <katie.snell@student.manchester.ac.uk>
2019-10-13 14:50:27 +0100

    Added datetime selector for event

--- client/App.js
+++ client/App.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { Image, StyleSheet, View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Card, Button, Icon } from 'react-native-elements';
+import DatePicker from 'react-native-datepicker'
 
 import server from './server';
 
@@ -31,7 +32,7 @@ class EventsScreen extends React.Component {
   
   componentDidMount() {
     server.getEvents().then(events => {
-      console.log(JSON.stringify(events))
+      // console.log(JSON.stringify(events))
       this.setState({events})
     });
   }
@@ -46,7 +47,7 @@ class EventsScreen extends React.Component {
           {
             this.state.events.map((u, i) => {
               return (
-                <Card title={u.name}>
+                <Card style={styles.card} title={u.name}>
                   <View key={i} style={styles.user}>
                     <Text>{u.location}</Text>
                     <Button
@@ -64,9 +65,15 @@ class EventsScreen extends React.Component {
 }
 
 class SelectedEventScreen extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = { selectedEta: '' }
+  }
+
   render() {
     const { navigation } = this.props;
-    console.log(JSON.stringify(navigation));
+    // console.log(JSON.stringify(navigation));
 
     const name = navigation.getParam('name', 'Bob');
     const location = navigation.getParam('location', 'Turing Tap, Manchester');
@@ -74,8 +81,21 @@ class SelectedEventScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
         <View style={styles.title}>
           <Text style={styles.titleText}>{name}</Text>
-          <Text style={styles.titleText}>{location}</Text>
         </View>
+        <Text>{location}</Text>
+
+        <DatePicker
+          style={{width: 300}}
+          date={this.state.selectedEta}
+          mode='datetime'
+          placeholder='What time will you arrive ⏱'
+          showIcon={false}
+          onDateChange={(selectedEta) => {
+            this.setState({selectedEta: selectedEta})
+            console.log(this.state.selectedEta)
+          }}
+        />
+
       </View>
     );
   }
@@ -84,7 +104,10 @@ class SelectedEventScreen extends React.Component {
 const styles = StyleSheet.create({
 	titleText: {
     fontSize: 28,
-	}
+  },
+  card: {
+    width: 400
+  }
 });
 
 const AppNavigator = createStackNavigator(
